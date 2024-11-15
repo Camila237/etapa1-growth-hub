e8ae2fa758114f35ebcc9c2a4669b204c5c6dfe0
CAMILA\camil <maria.rincon@venko.co>
2024-11-14 19:09:50 -0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:etapa1/config/exports.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _setPreferredOrientations();
   runApp(const MyApp());
 }
+void _setPreferredOrientations() => SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
